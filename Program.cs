d82eeb9fb67ae4c067e7c56c48d2533e5d32ccf7
Afevkaya <Afevkaya5151@gmail.com>
2022-01-21 02:12:07 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectOne // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            
+        }
+    }
+}

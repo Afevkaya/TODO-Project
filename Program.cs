73fb7075d778ca22f86c46a1287ae4c188607070
Afevkaya <Afevkaya5151@gmail.com>
2022-01-27 04:31:34 +0300

    add card metod added

--- Program.cs
+++ Program.cs
@@ -9,28 +9,73 @@ namespace ProjectTwo // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Board board = new Board();
+            AddCard(board);
             ListCards(board);
+            
         }
 
         //Kart Listeleme
         static void ListCards(Board board){
-            foreach (var todo in board.TODO)
+            if (board.TODO != null)
             {
-                Console.WriteLine(todo.Name);
+                foreach (var todo in board.TODO)
+                {
+                    Console.WriteLine(todo.Name + " "+todo.);
+                }
             }
-            foreach (var inprogress in board.INPROGRESS)
+            if (board.INPROGRESS != null)
             {
-                Console.WriteLine(inprogress.Name);
+                foreach (var inprogress in board.INPROGRESS)
+                {
+                    Console.WriteLine(inprogress.Name);
+                }
             }
-            foreach (var done in board.DONE)
+            if (board.DONE != null)
             {
-                Console.WriteLine(done.Name);
+                foreach (var done in board.DONE)
+                {
+                    Console.WriteLine(done.Name);
+                }
             }
         }
 
         //Kart Ekleme
         static void AddCard(Board board){
+            board.TODO  = new List<Card>();
+            Card card = new Card();
+
+            List<User> users = new List<User>{
+                new User{
+                    ID = 1,
+                    Name ="Ahmet"
+                },
+                new User{
+                    ID = 2,
+                    Name ="Enes"
+                },
+                new User{
+                    ID = 3,
+                    Name ="Kemal"
+                }
+            };
             
+            Console.WriteLine("Lutfen kart başlığını giriniz: ");
+            card.Name = Console.ReadLine();
+            
+            Console.WriteLine("Lutfen kart içeriğini giriniz: ");
+            card.Content = Console.ReadLine();
+            
+            Console.WriteLine("Lutfen kullaıcı seçiniz: ");
+            foreach (var user in users){
+                Console.WriteLine(user.ID + " " +user.Name);
+            }
+            card.UserId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Lutfen size seçiniz: XS(1) S(2) M(3) L(4) XL(5): ");
+            card.Size = Convert.ToInt32(Console.ReadLine());
+
+            board.TODO.Add(card);
+
         }
     }
 }
